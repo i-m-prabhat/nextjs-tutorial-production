a8b2e7d05eda7688d8ec1b04507b2244ca8e3531
john smilga <johnsmilga@gmail.com>
2023-11-11 10:55:25 -0500

    add 'force-dynamic'

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,8 +1,8 @@
 import TaskForm from '@/components/TaskForm';
 import TaskFormCustom from '@/components/TaskFormCustom';
 import TaskList from '@/components/TaskList';
-
-const TasksPage = () => {
+export const dynamic = 'force-dynamic';
+const TasksPage = async () => {
   return (
     <div className='max-w-lg'>
       <TaskFormCustom />
